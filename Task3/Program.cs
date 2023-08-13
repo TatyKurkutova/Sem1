50b1201226dd4d5ef9eeb385921688f36ce69f7b
Tatiana <tatianka.dmitrieva.96@mail.ru>
2023-08-14 00:47:37 +0300

    Find number

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет */
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+double number1 = double.Parse(Console.ReadLine()!);
+if (number1%2==0)
+   Console.WriteLine ($"Да");
+else 
+{
+   Console.WriteLine ($"Нет");
+}
